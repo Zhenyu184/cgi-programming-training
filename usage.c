6131844b9daf663e6635977ed3d3c6ad007d21ef
Zhenyu184 <zyy1120126@gmail.com>
2024-04-03 10:56:26 +0000

    feat(usage): parse pid and s parameters

--- usage.c
+++ usage.c
@@ -12,15 +12,36 @@
 // 取得核心數
 int get_cpu_cores() {
     int cores = sysconf(_SC_NPROCESSORS_ONLN);
-    // sysconf 如果錯誤會返回 -1 ，並設定 errno
-    // 為了避免除 0，令 cores = 0 返回 1
-    if (cores <= 0)
+    if (cores <= 0) {
         return 1;
+    }
     return cores;
 }
 
 // 實現 usage 功能
 int usage(INPUT *input) {
     printf("Content-Type:application/json; charset=utf-8\n\n");
+
+    // 檢查輸入指標
+    if (input == NULL) {
+        return 0;
+    }
+
+    // 如果pid沒有或不指定就是自己
+    unsigned long pid = getpid();
+    INPUT *pid_tmp = NULL;
+    if ((pid_tmp = CGI_Find_Parameter(input, "pid")) != NULL && pid_tmp->val && strlen(pid_tmp->val) != 0) {
+        pid = (unsigned long)atoi(pid_tmp->val);
+    }
+
+    // 如果s沒有或不指定就是自己
+    unsigned int s = 1;
+    INPUT *s_tmp = NULL;
+    if ((s_tmp = CGI_Find_Parameter(input, "s")) != NULL && s_tmp->val) {
+        s = (unsigned int)atoi(s_tmp->val);
+    }
+
+    printf("pid = %lu\n", pid);
+
     return 0;
 }
